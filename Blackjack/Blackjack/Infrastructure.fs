bf08066ef0009da38f00b38e2470534aaf2122bf
Cameron Presley <cameron@thesoftwarementor.com>
2016-05-29 22:28:39 -0400

    Added infrastructure.

---
+++ Blackjack/Blackjack/Infrastructure.fs
@@ -0,0 +1,17 @@
+﻿module Infrastructure
+open FSharp.Reflection
+
+let GetDiscriminatedUnionEnums<'a> () =
+    FSharpType.GetUnionCases(typeof<'a>)
+    |> Array.map (fun case -> FSharpValue.MakeUnion(case, [||]) :?> 'a)
+    |> Array.toList
+
+type MaybeBuilder() =
+    member this.Bind(x, f) = 
+        match x with
+        | Some x -> f x
+        | None -> None
+
+    member this.Return(x) = Some x
+
+
